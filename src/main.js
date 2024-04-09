f0bebb081e5b19d3d1c1042a5eda1de62ab8e5e8
Jordi <im.jordiparadelo@gmail.com>
2024-04-09 13:45:07 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-// import './styles/variables.css'
-// import './styles/style.css'
-// import './styles/themes.css'
+import './styles/variables.css'
+import './styles/style.css'
+import './styles/themes.css'
 
 import {initComponents} from "./libs/components/index.js";
 
 function init() {
 	initComponents();
-	console.log('working')
 }
 
 init()
