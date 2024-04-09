9de45702398a59189bbbc32ee1e379a55cbaa2d5
Jordi <im.jordiparadelo@gmail.com>
2024-04-09 11:05:25 +0200

    Initial commit

---
+++ src/libs/components/index.js
@@ -0,0 +1,12 @@
+export {default as autoScroll} from "./autoScroll";
+export {default as followMouseList} from "./followMouseList";
+export {default as contactForm} from "./contactForm";
+export {default as featuresWorks} from "./featuresWorks";
+export {default as pageIndex} from "./pageIndex";
+export {default as infiniteSlider} from "./infiniteSlider";
+export {default as projectApproach} from "./projectApproach";
+export {default as navMenu} from "./navMenu";
+export {default as pricingForm} from "./pricingForm";
+export {default as copyToClipboard} from "./copyToClipboard";
+// Init all components
+export {default as initComponents} from "./initComponents";
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './styles/style.css'
+
+import {initComponents} from "./libs/components/index";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+	initComponents();
+}
