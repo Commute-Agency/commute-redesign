f8e83aa832097f4050923340a3c118a559eb2f2f
Jordi <im.jordiparadelo@gmail.com>
2024-04-09 12:33:23 +0200

    Import updates

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './styles/variables.css'
+// import './styles/variables.css'
 import './styles/style.css'
-import './styles/themes.css'
+// import './styles/themes.css'
 
 import {initComponents} from "./libs/components/index.js";
-
-document.addEventListener("DOMContentLoaded", init);
 
 function init() {
 	initComponents();
 }
+
+init()
