0661d1d3052452a591b14b3c945da546f24b4e61
Jordi <im.jordiparadelo@gmail.com>
2024-04-09 13:29:48 +0200

    Update index.js

--- src/libs/components/index.js
+++ src/libs/components/index.js
@@ -1,12 +1,12 @@
-export {default as autoScroll} from "./autoScroll";
-export {default as followMouseList} from "./followMouseList";
-export {default as contactForm} from "./contactForm";
-export {default as featuresWorks} from "./featuresWorks";
-export {default as pageIndex} from "./pageIndex";
-export {default as infiniteSlider} from "./infiniteSlider";
-export {default as projectApproach} from "./projectApproach";
-export {default as navMenu} from "./navMenu";
-export {default as pricingForm} from "./pricingForm";
-export {default as copyToClipboard} from "./copyToClipboard";
+export {default as autoScroll} from "./autoScroll.js";
+export {default as followMouseList} from "./followMouseList.js";
+export {default as contactForm} from "./contactForm.js";
+export {default as featuresWorks} from "./featuresWorks.js";
+export {default as pageIndex} from "./pageIndex.js";
+export {default as infiniteSlider} from "./infiniteSlider.js";
+export {default as projectApproach} from "./projectApproach.js";
+export {default as navMenu} from "./navMenu.js";
+export {default as pricingForm} from "./pricingForm.js";
+export {default as copyToClipboard} from "./copyToClipboard.js";
 // Init all components
-export {default as initComponents} from "./initComponents";
+export {default as initComponents} from "./initComponents.js";
